ff492c25247a4d8cf471eb7f5008f8345329c9e7
Graeme Blair <graemeblair@users.noreply.github.com>
2026-02-02 22:21:36 -0800

    ed

--- code/download-new-dtm.R
+++ code/download-new-dtm.R
@@ -11,7 +11,17 @@ url <- "https://www.ice.gov/detain/detention-management"
 doc <-
   request(url) |>
   req_user_agent(
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.32 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.32"
+    str_c(
+      "Mozilla/5.0 (X11; Linux x86_64)",
+      "AppleWebKit/537.36 (KHTML, like Gecko)",
+      "Chrome/121.0.0.0 Safari/537.36"
+    )
+  ) |>
+  req_headers(
+    "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language" = "en-US,en;q=0.9",
+    "Cache-Control" = "no-cache",
+    "Pragma" = "no-cache"
   ) |>
   req_timeout(1) |>
   req_perform() |>
