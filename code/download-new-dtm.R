2c17260541cf80b49286a6c27616f538e15ade45
Graeme Blair <graemeblair@users.noreply.github.com>
2026-03-14 22:03:02 -0700

    expand data collection

--- code/download-new-dtm.R
+++ code/download-new-dtm.R
@@ -10,24 +10,12 @@ url <- "https://www.ice.gov/detain/detention-management"
 # 1) Download the HTML reliably (libcurl), then parse it
 doc <-
   request(url) |>
-  req_user_agent(
-    str_c(
-      "Mozilla/5.0 (X11; Linux x86_64)",
-      "AppleWebKit/537.36 (KHTML, like Gecko)",
-      "Chrome/121.0.0.0 Safari/537.36"
-    )
-  ) |>
-  req_headers(
-    "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language" = "en-US,en;q=0.9",
-    "Cache-Control" = "no-cache",
-    "Pragma" = "no-cache"
-  ) |>
-  req_timeout(1) |>
+  req_timeout(30) |>
+  req_retry(max_tries = 3, backoff = ~5) |>
   req_perform() |>
   resp_body_html()
 
-# 2) Extract the XLSX link (and make it absolute if it’s relative)
+# 2) Extract the XLSX link (and make it absolute if it's relative)
 link <-
   doc |>
   html_elements("a") |>
@@ -40,7 +28,8 @@ link <-
 
 stopifnot(!is.na(link))
 
-fname <- basename(link)
+# Strip query parameters from filename to avoid polluted filenames
+fname <- basename(link) |> str_remove("\\?.*$")
 dir.create("spreadsheets", showWarnings = FALSE, recursive = TRUE)
 
 dest <- file.path("spreadsheets", fname)
